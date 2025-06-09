fec8b3fc827d83151f1ab75b1a6895714054ae4a
Alessandro Candido <candido.ale@gmail.com>
2025-06-09 13:39:25 +0200

    build: Add nix shell

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    systems.url = "github:nix-systems/default";
+  };
+
+  outputs = {
+    nixpkgs,
+    systems,
+    ...
+  }: let
+    forEachSystem = nixpkgs.lib.genAttrs (import systems);
+  in {
+    devShells =
+      forEachSystem
+      (system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            stdenv
+            gnumake
+            ccls
+            # clang
+            gcc
+            pkg-config
+            cmake
+            meson
+            ninja
+          ];
+        };
+      });
+  };
+}
+
